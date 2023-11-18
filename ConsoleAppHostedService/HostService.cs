98df26def62825338b994264f9390e749ea538b1
Alam Alfaris <alamalfaris97@gmail.com>
2023-11-18 09:15:36 +0700

    Add project files.

---
+++ ConsoleAppHostedService/HostService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppHostedService
+{
+    public class HostService : IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Method: StartAsync");
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Method: StopAsync");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ ConsoleAppHostedService/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.ServiceProcess;
+
+namespace ConsoleAppHostedService
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, service) =>
+                {
+                    service.AddHostedService<HostService>();
+
+                    // add other service
+                });
+        }
+    }
+}
